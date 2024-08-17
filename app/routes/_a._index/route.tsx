060df4d81ac213c1a4d83dec3780ce953a769534
berk ziya <berkziya1@gmail.com>
2024-08-17 13:48:02 +0300

    oley

--- app/routes/_a._index/route.tsx
+++ app/routes/_a._index/route.tsx
@@ -1,11 +1,8 @@
 import { ActionFunctionArgs, json } from '@remix-run/node';
 import { Form, useActionData } from '@remix-run/react';
+import { UserContext } from 'ozen-bot/dist/UserContext';
 import invariant from 'tiny-invariant';
 import ClientHandler from '~/.server/clientHandler';
-
-export async function loader() {
-  return null;
-}
 
 export async function action({ request }: ActionFunctionArgs) {
   const client = (await ClientHandler.getInstance()).getClient();
@@ -14,16 +11,25 @@ export async function action({ request }: ActionFunctionArgs) {
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
 
-  const user = await client.createUserContext();
-  const result = await user.login(email, password);
+  try {
+    const user = await client.createUserContext();
+    const id = await user.login(email, password);
 
-  invariant(result, 'Login failed');
-  const users = [...client.users];
-  return json({ users });
+    invariant(id, 'Login failed');
+    const users = [...client.users];
+    return json({ users, id });
+  } catch (error) {
+    return json({ error: (error as Error).message }, { status: 400 });
+  }
 }
 
 export default function Login() {
-  const users = useActionData();
+  const data = useActionData<{
+    error?: string;
+    users?: any[];
+    id?: number;
+  }>();
+  const newUser = data?.users?.find((x) => x.id == data.id);
   return (
     <div className='flex flex-col items-center m-12'>
       <Form method='post' className='flex flex-col gap-4'>
@@ -31,7 +37,8 @@ export default function Login() {
         <input type='password' name='password' className='' />
         <button type='submit'>Submit</button>
       </Form>
-      {users ? <p>{JSON.stringify(users, null, 2)}</p> : null}
+      {data?.error && <p className='text-red-500'>{data.error}</p>}
+      {newUser && <p>{JSON.stringify(newUser, null, 2)}</p>}
     </div>
   );
 }
---
+++ app/routes/_a.data.parselist/route.tsx
@@ -0,0 +1,74 @@
+import { ActionFunctionArgs } from '@remix-run/node';
+import { Form, useActionData } from '@remix-run/react';
+import {
+  getCitizenList,
+  getResidentList,
+  getStateCitizens,
+  getStateResidents,
+  getWarDamageList,
+} from 'ozen-bot/dist/baseFunctions/getInfo/misc/getPlayerList';
+import ClientHandler from '~/.server/clientHandler';
+
+export async function action({ request }: ActionFunctionArgs) {
+  const user = await (await ClientHandler.getInstance()).getUser();
+  const formData = await request.formData();
+  const list = formData.get('list') as string;
+  const id = parseInt(formData.get('id') as string);
+  let players: any[] = [];
+  switch (list) {
+    case 'citizenList':
+      players = await getCitizenList(user, id);
+      break;
+    case 'residentList':
+      players = await getResidentList(user, id);
+      break;
+    case 'stateCitizens':
+      players = await getStateCitizens(user, id);
+      break;
+    case 'stateResidents':
+      players = await getStateResidents(user, id);
+      break;
+    case 'warDef':
+      players = await getWarDamageList(user, id, false);
+      break;
+    case 'warAttack':
+      players = await getWarDamageList(user, id, true);
+      break;
+  }
+  return { players };
+}
+
+export default function Parser() {
+  const actionData = useActionData<typeof action>();
+  return (
+    <div className='flex flex-col bg-gray-300'>
+      <h1 className='text-2xl text-center'>Parse List</h1>
+      <Form method='post'>
+        <select name='list' className='border p-1'>
+          <option value='citizenList'>Citizen List</option>
+          <option value='residentList'>Resident List</option>
+          <option value='stateCitizens'>State Citizens</option>
+          <option value='stateResidents'>State Residents</option>
+          <option value='warDef'>War Defenders</option>
+          <option value='warAttack'>War Attackers</option>
+        </select>
+        <input type='text' name='id' className='border p-1' />
+        <button type='submit' className='p-2 bg-blue-500 text-white'>
+          Parse
+        </button>
+      </Form>
+      {actionData?.players && (
+        <div>
+          {actionData.players.map((player) => (
+            <div key={player.id} className='border p-1 flex justify-between'>
+              <div className='font-bold'>{player.id}</div>
+              <div>{player.name}</div>
+              <div>{player.level}</div>
+              <div>{player.damage}</div>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
